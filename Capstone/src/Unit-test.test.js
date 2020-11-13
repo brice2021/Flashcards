0d3755e62d2767058ec9886be8194a14aacb2b73
Brittanie Rice <71941423+brice2021@users.noreply.github.com>
2020-11-13 15:10:26 -0600

    Delete Unit-test.test.js

--- Capstone/src/Unit-test.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('sucessfully render all 3 components without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
